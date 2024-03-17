bf3b3d2f2c9b6fc6405e9e9e322b98ad9263aac2
Soniya Vijayakumar <44982216+vksoniya@users.noreply.github.com>
2024-03-17 17:21:05 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,10 @@
+from sklearn.manifold import TSNE
 
+def reduce_dim__per_token(input_vector):
+tsne_dim_reducer = TSNE(n_components=2, perplexity=5)
+    
+	#tsne
+	tsne_dim_reduced_vector = tsne_dim_reducer.fit_transform(np.array(input_vector))
+	tsne_df_dim_reduced = pd.DataFrame.from_dict({'x':tsne_dim_reduced_vector[:,0],'y':tsne_dim_reduced_vector[:,1]})
+    
+return tsne_df_dim_reduced
